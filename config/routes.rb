633f38f9c9458050b15a0255c01a51dd96e46909
Christin Cech <christin.cech@gmail.com>
2019-10-16 02:36:46 +0000

    Create Model, Data Migration, Simple Form Gem, and Build New Form for Item

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  root 'places#index'
+
+  #root 'places#index'
+  root 'posts#index'
+  resources :posts 
+  
 end
 
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
